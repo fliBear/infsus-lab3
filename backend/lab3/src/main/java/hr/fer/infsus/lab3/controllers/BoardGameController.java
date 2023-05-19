e6d23e72a49576d8353720f43aaa26997744cef5
David Garić <david.garic@fer.hr>
2023-05-19 13:37:02 +0200

    added routes for board game controller

--- backend/lab3/src/main/java/hr/fer/infsus/lab3/controllers/BoardGameController.java
+++ backend/lab3/src/main/java/hr/fer/infsus/lab3/controllers/BoardGameController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +27,7 @@ public class BoardGameController {
 		this.bgService = bgService;
 	}
 	
+	@GetMapping("/boardGame/{id}")
 	public ResponseEntity<BoardGame> getBoardGame(@PathVariable Long id) {
 		BoardGame boardGame = bgService.getBoardGame(id);
         if(boardGame == null){
@@ -31,16 +36,19 @@ public class BoardGameController {
         return ResponseEntity.status(HttpStatus.OK).body(boardGame);
 	}
 	
+	@GetMapping("/boardGames")
 	public ResponseEntity<List<BoardGame>> getAllBoardGames() {
 		List<BoardGame> boardGames = bgService.getAllBoardGames();
         return ResponseEntity.status(HttpStatus.OK).body(boardGames);
 	}
 	
+	@DeleteMapping ("/deleteBoardGame/{id}")
 	public ResponseEntity<Void> deleteBoardGame(@PathVariable Long id) {
 		bgService.deleteBoardGame(id);
         return ResponseEntity.noContent().build();
 	}
 	
+	@PostMapping("/createBoardGame")
 	public ResponseEntity<BoardGame> createBoardGame(@RequestBody BoardGame boardGame) {
 		boolean isCreated = bgService.createBoardGame(boardGame);
         if (isCreated==false) {
@@ -49,6 +57,7 @@ public class BoardGameController {
         return ResponseEntity.status(HttpStatus.CREATED).body(boardGame);
 	}
 	
+	@PutMapping("/editBoardGame/{id}")
 	public ResponseEntity<BoardGame> editBoardGame(@PathVariable Long id, @RequestBody BoardGame boardGame) {
 		boolean isEdited = bgService.editBoardGame(id, boardGame);
         if (!isEdited) {
